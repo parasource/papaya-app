06df2ff58bde7ce4ea510982c7f196feddc8ae37
Matveyx25 <matveyx25@gmail.com>
2023-06-17 16:17:10 +0400

    English language

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,10 +1,11 @@
 import * as Localization from 'expo-localization';
 import { I18n } from 'i18n-js';
 import { ru } from '../locales/ru';
+import { en } from '../locales/en';
 
 export const i18n = new I18n({
-	ru: ru
+	ru: ru,
+	en: en
 })
 i18n.enableFallback = true
-// i18n.locale = Localization.locale
-i18n.locale = 'ru'
+i18n.locale = Localization.locale
---
+++ locales/en.js
@@ -0,0 +1,106 @@
+export const en = {
+  tabbar: {
+		home: 'Home',
+		search: 'Search',
+		bookmarked: 'Likes',
+		profile: 'Profile',
+	},
+	home: {
+		search: 'Search',
+		update: 'Need an update',
+		topic: 'TOPIC',
+		article: 'ARTICLE',
+		forYou: 'For you'
+	},
+	profile: {
+		title: 'Profile',
+		settingsSubtitle: 'Change login and gender',
+		myWardrobeTitle: 'My Wardrobe',
+		myWardrobeSubtitle: 'Edit your wardrobe to improve your recommendations',
+		pushTitle: 'Push-notification',
+		support: 'Write to support',
+		logout: 'Sign out',
+		remove: 'Remove account',
+		alertLogoutTitle: 'Are you sure?',
+		alertLogoutSubtitle: 'Are you sure you want to sign out?',
+		alertRemoveTitle: 'Are you sure?',
+		alertRemoveSubtitle: 'Are you sure you want to remove account?',
+		alertCancel: 'Cancel',
+		alertLogout: 'Sign out',
+		alertRemove: 'Remove',
+	},
+	settings: {
+		userName: 'User name',
+		name: 'Name',
+		settingsData: 'Credentials',
+		yourSex: 'Your gender',
+		male: 'Male',
+		female: 'Female',
+		cancel: 'Cancel',
+		sheetTitle: 'Choose your gender'
+	},
+	wardrobe: {
+		empty: 'Your wardrobe is empty \nAdd wardrobe items',
+		addItem: 'Add clothes',
+		countItems: 'things',
+	},
+	headerTitles: {
+		wardrobe: 'Wardrobe',
+		myWardrobe: 'My Wardrobe',
+		profile: 'Account',
+	},
+	item: {
+		buyInShop: 'Buy in the store',
+		looksWith: 'Images with this thing',
+		shop: ' shop',
+		shops: ' shops',
+		shops2: ' shops',
+	},
+	lookBottomSheet: {
+		title: 'The look is selected based on your wardrobe',
+		subtitle: 'This icon indicates looks that have been recommended based on your wardrobe. They have at least one thing that you have.',
+		button: 'Great!'
+	},
+	favorite: {
+		title: 'Likes',
+	},
+	looksFeed: {
+		noMore: 'No more looks'
+	},
+	search: {
+		popular: 'Popular this week',
+		recently: 'Recently',
+		clear: 'Clear',
+		cancel: 'Cancel',
+		blurPlaceholder: 'Search',
+		focusPlaceholder: 'Shirt with trousers',
+		searchLooks: 'Search looks',
+		buyInShop: 'Buy in the store',
+		notBuyInShop: "We haven't found where to buy this item yet",
+		otherSearch: 'Also looking for',
+		searchByItem: 'Search by item',
+		result: 'Search result',
+		noResult: 'No results were found for your search',
+	},
+	welcome: {
+		title: 'Dressing Stylishly Simple',
+		subtitle: 'Choose your look from a thousand available',
+	},
+	genderSelect: {
+		title: 'Enter your gender',
+		subtitle: 'We will show you personal looks. Gender can be changed later in the settings',
+		or: 'Or',
+		male: 'Guy',
+		female: 'Girl',
+	},
+	look: {
+		like: 'Like',
+		author: 'Image author',
+		items: 'Image elements',
+		notFound: "We haven't found the item in the photo yet, but we will soon!",
+		lookLike: 'Similar looks',
+		searchLooks: 'Search Looks',
+		buyInShop: 'Buy in shop',
+		shopNotFound: "We haven't found a place to buy this item yet",
+	}
+}
